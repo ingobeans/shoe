14adb797340e4c7ccc2e3afd5fc702830d0c43f2
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-01-24 17:02:00 +0100

    initial command

---
+++ src/main.rs
@@ -0,0 +1,158 @@
+use crossterm::{
+    cursor::{MoveLeft, MoveRight},
+    event::{self, Event, KeyCode, KeyEventKind, KeyModifiers},
+    queue,
+    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
+};
+use std::{
+    io::{stdin, stdout, Read, Result, Write},
+    path::PathBuf,
+};
+
+struct Ash {
+    running: bool,
+    listening: bool,
+    cwd: PathBuf,
+    input_text: String,
+    cursor_pos: usize,
+}
+
+impl Ash {
+    fn new() -> Result<Self> {
+        Ok(Ash {
+            running: false,
+            listening: false,
+            cwd: std::env::current_dir()?,
+            input_text: String::new(),
+            cursor_pos: 0,
+        })
+    }
+    fn handle_command(&mut self, parts: Vec<String>) -> Result<()> {
+        println!("you wrote: {:?}", parts);
+        Ok(())
+    }
+    fn write_char(&mut self, new_char: char) {
+        if self.input_text.chars().count() == self.cursor_pos {
+            self.input_text.insert(self.input_text.len(), new_char);
+            return;
+        } else if self.cursor_pos == 0 {
+            self.input_text.insert(0, new_char);
+            return;
+        }
+        let mut new = String::new();
+        for (index, char) in self.input_text.chars().enumerate() {
+            new.insert(new.len(), char);
+            if index == self.cursor_pos - 1 {
+                new.insert(new.len(), new_char);
+            }
+        }
+        self.input_text = new;
+    }
+    fn delete_char(&mut self) {
+        let mut new = String::new();
+        for (index, char) in self.input_text.chars().enumerate() {
+            if index != self.cursor_pos {
+                new.insert(new.len(), char);
+            }
+        }
+        self.input_text = new;
+    }
+    fn handle_key_press(&mut self, event: Event) -> Result<()> {
+        if let Event::Key(key_event) = event {
+            if key_event.kind != KeyEventKind::Press {
+                return Ok(());
+            }
+            match key_event.code {
+                KeyCode::Enter => {
+                    self.listening = false;
+                }
+                KeyCode::Char(char) => {
+                    if key_event.modifiers.contains(KeyModifiers::CONTROL) && char == 'c' {
+                        self.listening = false;
+                        self.running = false;
+                        return Ok(());
+                    }
+                    self.write_char(char);
+                    self.cursor_pos += 1;
+                }
+                KeyCode::Delete => {
+                    self.delete_char();
+                }
+                KeyCode::Backspace => {
+                    if self.cursor_pos > 0 {
+                        self.cursor_pos -= 1;
+                        self.delete_char();
+                    }
+                }
+                KeyCode::Right => {
+                    self.cursor_pos += 1;
+                    if self.cursor_pos > self.input_text.chars().count() {
+                        self.cursor_pos = self.input_text.chars().count();
+                    }
+                }
+                KeyCode::Left => {
+                    self.cursor_pos = self.cursor_pos.saturating_sub(1);
+                }
+                KeyCode::Home => {
+                    self.cursor_pos = 0;
+                }
+                KeyCode::End => {
+                    self.cursor_pos = self.input_text.chars().count();
+                }
+                _ => {}
+            }
+            self.update()?;
+        }
+        Ok(())
+    }
+    fn update(&self) -> Result<()> {
+        queue!(stdout(), Clear(ClearType::UntilNewLine))?;
+        print!("{}", self.input_text);
+        if self.input_text.chars().count() != 0 {
+            queue!(stdout(), MoveLeft((self.input_text.chars().count()) as u16))?;
+        }
+
+        if self.cursor_pos != 0 {
+            queue!(stdout(), MoveRight(self.cursor_pos as u16))?;
+        }
+
+        stdout().flush()?;
+
+        if self.cursor_pos != 0 {
+            queue!(stdout(), MoveLeft(self.cursor_pos as u16))?;
+        }
+
+        Ok(())
+    }
+    fn start(&mut self) -> Result<()> {
+        self.running = true;
+        while self.running {
+            let command = vec![self.listen()?];
+            self.handle_command(command)?;
+        }
+        Ok(())
+    }
+    fn listen(&mut self) -> Result<String> {
+        enable_raw_mode()?;
+        self.listening = true;
+        print!(">");
+        stdout().flush()?;
+        while self.listening {
+            self.handle_key_press(event::read()?)?;
+        }
+        if self.input_text.chars().count() != 0 {
+            queue!(stdout(), MoveRight(self.input_text.chars().count() as u16))?;
+        }
+        println!("\n");
+        disable_raw_mode()?;
+        let text = self.input_text.clone();
+        self.input_text = String::new();
+        self.cursor_pos = 0;
+        Ok(text)
+    }
+}
+
+fn main() {
+    let mut ash = Ash::new().unwrap();
+    ash.start().unwrap();
+}
