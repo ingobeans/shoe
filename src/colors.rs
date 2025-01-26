1cecd4b8e00e40a86d13ac0490f1c572501be9ef
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-01-26 19:34:24 +0100

    nicer colors (very important)

--- src/colors.rs
+++ src/colors.rs
@@ -2,14 +2,14 @@ use crossterm::style::Color;
 
 pub static PRIMARY_COLOR: Color = Color::Rgb {
     r: 255,
-    g: 173,
-    b: 228,
+    g: 193,
+    b: 69,
 };
 
 pub static SECONDARY_COLOR: Color = Color::Rgb {
-    r: 173,
-    g: 226,
-    b: 255,
+    r: 91,
+    g: 95,
+    b: 151,
 };
 
 pub static ERR_COLOR: Color = Color::Red;
