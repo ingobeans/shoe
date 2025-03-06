c46e79727b078cae7de6ef88a3b65d4ba58c1a59
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-03-06 21:10:54 +0100

    quote reconstruction didnt work as intended :<

--- src/main.rs
+++ src/main.rs
@@ -1133,14 +1133,9 @@ fn main() {
     // will be Some if the -c or -k argument has been hit, if so, all following args are appended to this
     let mut run_command: Option<String> = None;
     let mut exit_after_run_command = false;
-    for mut arg in args.skip(1) {
+    for arg in args.skip(1) {
         // if -c or -k has been hit, simply append this arg to run_command
         if let Some(run_command) = &mut run_command {
-            // if arg has spaces, wrap it in quotes so its parsed correctly (yes this is sort of lazy but better than nothing)
-            if arg.contains(' ') && !arg.contains('"') {
-                arg = format!("\"{arg}\"");
-            }
-            // append
             *run_command += &(arg + " ");
         } else {
             match arg.as_str() {
