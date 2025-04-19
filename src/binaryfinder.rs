3e6a71129130753457ba039e4057c757ad9f1370
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-04-20 01:17:30 +0200

    more docs

--- src/binaryfinder.rs
+++ src/binaryfinder.rs
@@ -5,6 +5,7 @@
 use relative_path::RelativePathBuf;
 use std::{collections::HashMap, env, io::Result, path::PathBuf};
 
+/// Get executable name used for running certain script file extension
 pub fn get_script_runtime(script_extension: &str) -> Option<&str> {
     Some(match script_extension {
         "py" => "python3",
@@ -14,6 +15,7 @@ pub fn get_script_runtime(script_extension: &str) -> Option<&str> {
     })
 }
 
+/// Get executable file extensions
 pub fn get_path_extensions() -> Vec<String> {
     if env::consts::OS != "windows" {
         Vec::new()
@@ -26,6 +28,7 @@ pub fn get_path_extensions() -> Vec<String> {
     }
 }
 
+/// Generate variants of a path. On windows, will return variants with .exe, .cmd and .bat extensions added.
 pub fn get_path_variants(path: &str, path_extensions: &Vec<String>) -> Vec<String> {
     if env::consts::OS != "windows" {
         vec![path.to_owned()]
@@ -40,6 +43,7 @@ pub fn get_path_variants(path: &str, path_extensions: &Vec<String>) -> Vec<Strin
     }
 }
 
+/// Get all items in path as a hashmap, with key being the filename, and value the path
 pub fn get_items_in_path() -> HashMap<String, PathBuf> {
     let path = env::var_os("PATH").unwrap_or_default();
     let paths = env::split_paths(&path).collect::<Vec<_>>();
@@ -64,6 +68,7 @@ pub fn get_items_in_path() -> HashMap<String, PathBuf> {
     items
 }
 
+/// Finds an executable binary from a name/path
 pub fn find_binary(
     input: &str,
     path_items: &HashMap<String, PathBuf>,
